c06d90b7eed0aeca0d8cdc9e87d3d353c62bc82c
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-03-02 06:10:34 +0200

    feat: Implement CaesarCipher class in Ciphers package.

--- src/Ciphers/CaesarCipher.java
+++ src/Ciphers/CaesarCipher.java
@@ -1,5 +1,40 @@
 package Ciphers;
 
-public class CaesarCipher {
-    
+public final class CaesarCipher {
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";    
+    private static final int ALPHABET_SIZE = ALPHABET.length();
+
+    public static String encrypt(String plaintext, int key) {
+        return applyCaesarCipher(plaintext, key, true);
+    }
+
+    public static String decrypt(String ciphertext, int key) {
+        return applyCaesarCipher(ciphertext, key, false);
+    }
+
+    private static String applyCaesarCipher(String text, int key, boolean isEncryption){
+        int shift = isEncryption ? key : -key;
+        
+        var result = new StringBuilder(text.length());
+        
+        for (char character : text.toCharArray()){
+            if (!Character.isLetter(character)){
+                result.append(character);
+            }
+            else {
+                int index = ALPHABET.indexOf(Character.toLowerCase(character));
+                /** 
+                // 'a' is subtracted to convert ASCII value to alphabet index
+                // int index = Character.toLowerCase(character) - 'a';
+                */
+                int shiftedIndex = Math.floorMod(index + shift, ALPHABET_SIZE);
+                
+                char shiftedCharacter = ALPHABET.charAt(shiftedIndex);
+                shiftedCharacter = Character.isUpperCase(character) ? Character.toUpperCase(shiftedCharacter) : shiftedCharacter;
+
+                result.append(shiftedCharacter);
+            }
+        }
+        return result.toString();
+    }    
 }
