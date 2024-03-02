3ba63fcc4f2620c7099a937667e19bd6083ae99d
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-03-02 06:11:33 +0200

    test: Verify CaesarCipher functionality in Main class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import Ciphers.CaesarCipher;
+
 public class Main {
     public static void main(String[] args) {
-        
+        String encryptedText = CaesarCipher.encrypt("Mahmoud Ahmed", 5);
+        System.out.println("EncryptedText: " + encryptedText);
+        System.out.println(CaesarCipher.decrypt(encryptedText, 5));
     }
 }
