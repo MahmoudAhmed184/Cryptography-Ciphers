9c03261f53662f8dc1a2beb3de2ccae504a701f5
Mahmoud Ahmed <ma7moudbahnasawy@outlook.com>
2024-03-18 13:15:12 +0200

    refactor: Simplify applyCaesarCipher method in CaesarCipher class.

--- src/Ciphers/CaesarCipher.java
+++ src/Ciphers/CaesarCipher.java
@@ -5,26 +5,22 @@ public final class CaesarCipher {
     private static final int ALPHABET_SIZE = ALPHABET.length();
 
     public static String encrypt(String plaintext, int key) {
-        return applyCaesarCipher(plaintext, key, true);
+        return applyCaesarCipher(plaintext, key);
     }
 
     public static String decrypt(String ciphertext, int key) {
-        return applyCaesarCipher(ciphertext, key, false);
+        return applyCaesarCipher(ciphertext, -key);
     }
 
-    private static String applyCaesarCipher(String text, int key, boolean isEncryption){
-        int shift = isEncryption ? key : -key;
-        
-        var result = new StringBuilder(text.length());
-        
-        for (char character : text.toCharArray()){
-            if (!Character.isLetter(character)){
-                result.append(character);
+    private static String applyCaesarCipher(String text, int shift) {
+        StringBuilder result = new StringBuilder(text.length());
+        for (char character : text.toCharArray()) {
+            if (Character.isLetter(character)) {
+                char shiftedCharacter = shiftCharacter(character, shift);
+                result.append(shiftedCharacter);
             }
             else {
-                char shiftedCharacter = shiftCharacter(character, shift);
-
-                result.append(shiftedCharacter);
+                result.append(character);
             }
         }
         return result.toString();
@@ -32,9 +28,7 @@ public final class CaesarCipher {
 
     private static char shiftCharacter(char character, int shift) {
         int index = ALPHABET.indexOf(Character.toLowerCase(character));
-        
         int shiftedIndex = Math.floorMod(index + shift, ALPHABET_SIZE);
-        
         char shiftedCharacter = ALPHABET.charAt(shiftedIndex);
         return Character.isUpperCase(character) ? Character.toUpperCase(shiftedCharacter) : shiftedCharacter;
     }
